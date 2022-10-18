087a509df1353adcdc4307ae664967de0a896799
Tryo Asnafi <tryoasnafi@gmail.com>
2022-10-18 17:11:33 +0700

    guessing game

---
+++ guessing_game/src/main.rs
@@ -0,0 +1,22 @@
+use std::io::{self, Write};
+
+// entry point
+fn main() {
+    println!("Tebak angka!");
+    print!("Masukkan angka: ");
+    io::stdout().flush().unwrap(); // ketika tidak di-flush, tidak akan muncul di terminal
+
+    /*
+        let -> keyword untuk deklarasi variabel
+        mut -> keyword untuk membuat variabel mutable (dapat di-reassign)
+        guess -> nama variabel
+    */
+    let mut guess = String::new();
+
+    // & -> operator untuk mengambil referensi dari variabel
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Gagal membaca inputan!");
+
+    println!("Angka yang kamu masukkan: {}", guess);
+}
