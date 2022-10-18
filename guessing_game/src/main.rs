cbe030a9885335b0673a7810761da1c70573e689
Tryo Asnafi <tryoasnafi@gmail.com>
2022-10-18 18:42:21 +0700

    update game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,24 +1,55 @@
-use std::io::{self, Write};
+use std::{cmp::Ordering, io};
+
+use rand::Rng;
 
 // entry point
 fn main() {
     println!("Tebak angka!");
-    print!("Masukkan angka: ");
-    io::stdout().flush().unwrap(); // ketika tidak di-flush, tidak akan muncul di terminal
 
-    /*
-        let -> keyword untuk deklarasi variabel
-        mut -> keyword untuk membuat variabel mutable (dapat di-reassign)
-        guess -> nama variabel
-    */
-    let mut guess = String::new();
+    loop {
+        // generating random number
+        let secret_number = rand::thread_rng().gen_range(1..=10);
+        println!("Masukkan angka: ");
+        // io::stdout().flush().unwrap(); // ketika print! tidak di-flush, tidak akan muncul di terminal langsung (buffered)
 
-    // & -> operator untuk mengambil referensi dari variabel
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Gagal membaca inputan!");
+        /*
+            let -> keyword untuk deklarasi variabel
+            mut -> keyword untuk membuat variabel mutable (dapat di-reassign)
+            guess -> nama variabel
+        */
+        let mut guess = String::new();
 
-    println!("Angka yang kamu masukkan: {}", guess);
+        // & -> operator untuk mengambil referensi dari variabel
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Gagal membaca inputan!");
+
+        // trim -> menghilangkan whitespace di awal dan akhir string
+        // parse -> mengubah string menjadi target type
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("❗Harap masukkan angka :");
+                continue;
+            }
+        };
+
+        // lakukan perbandingan antara inputan dan angka acak
+        // cmp() -> method untuk membandingkan 2 nilai
+        // Ordering -> enum yang berisi 3 nilai: Less, Greater, Equal
+        // match -> keyword untuk melakukan pattern matching
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Terlalu kecil!"),
+            Ordering::Greater => println!("Terlalu besar!"),
+            Ordering::Equal => {
+                println!("Benar! Anda menang!");
+                break;
+            }
+        }
+
+        println!("Angka yang kamu masukkan: {}", guess);
+        println!("Angka rahasia: {}\n", secret_number);
+    }
 }
 
 // flush explanation
